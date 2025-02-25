8ef648d1633d5758d21f5b97136ed8211cb9949f
Paulem <66211574+Paulem79@users.noreply.github.com>
2025-02-25 17:46:17 +0100

    Switched to basic versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,7 @@ buildscript {
 }
 
 group = "ovh.paulem.btm"
-val baseVersion = "2.6.7"
-version = "${baseVersion}.${getVersionSuffix()}"
+version = "2.6.7"
 
 // ------------------------ REPOSITORIES ------------------------
 repositories {
@@ -203,29 +202,6 @@ modrinth {
     uploadFile.set(tasks.shadowJar.get().archiveFile.get().asFile)
     gameVersions.addAll(listOf("1.21.4", "1.21.3", "1.21.2", "1.21.1", "1.21", "1.20.6", "1.20.5", "1.20.4", "1.20.3", "1.20.2", "1.20.1", "1.20", "1.19.4", "1.19.3", "1.19.2", "1.19.1", "1.19", "1.18.2", "1.18.1", "1.18", "1.17.1", "1.17", "1.16.5", "1.16.4", "1.16.3", "1.16.2", "1.16.1", "1.16", "1.15.2", "1.15.1", "1.15", "1.14.4", "1.14.3", "1.14.2", "1.14.1", "1.14", "1.13.2", "1.13.1", "1.13", "1.12.2", "1.12.1", "1.12", "1.11.2", "1.11.1", "1.11", "1.10.2", "1.10.1", "1.10", "1.9.4", "1.9.3", "1.9.2", "1.9.1", "1.9"))
     loaders.addAll(listOf("bukkit", "folia", "paper", "purpur", "spigot"))
-}
-
-fun getVersionSuffix(): String {
-    val stdout = ByteArrayOutputStream()
-    exec {
-        commandLine("git", "describe", "--tags", "--dirty", "--broken")
-        standardOutput = stdout
-    }
-    var output = stdout.toString().trim()
-    var suffix = ""
-    if (output.endsWith("-dirty")) {
-        output = output.substring(0, output.length - "-dirty".length)
-        suffix = "-dirty"
-    }
-    if (output.endsWith("-broken")) {
-        output = output.substring(0, output.length - "-broken".length)
-        suffix = "-broken"
-    }
-    if ('-' !in output) {
-        return "0$suffix"
-    }
-    val split = output.split('-')
-    return split[split.size - 2] + suffix
 }
 
 // ------------------------ MISC ------------------------
